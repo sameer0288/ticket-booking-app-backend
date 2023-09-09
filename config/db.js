00d069988443c052bd55e5203fa1dd55d4215b72
sameer0288 <sameersheikh0288@gmail.com>
2023-09-09 13:27:19 +0530

    first commit-2

--- config/db.js
+++ config/db.js
@@ -1,5 +1,7 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 require("dotenv").config();
-mongoose.set('strictQuery',false);
-const connecttion=mongoose.connect(process.env.mongourl);
-module.exports=connecttion
+mongoose.set("strictQuery", false);
+const connecttion = mongoose.connect(
+  process.env.mongourl || "mongodb://localhost:27017/ticket-booking"
+);
+module.exports = connecttion;
